c3be1f97e306552bfaae7ba30e233a3bbec9a34b
Daniel Sasse <dsasse07@gmail.com>
2021-03-03 14:06:00 -0500

    MGoogle Map integrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MapSection from './components/map/Map' // import the map here
 
 function App() {
+
+
+  const location = {
+    address: '1600 Amphitheatre Parkway, Mountain View, california.',
+    lat: 37.42216,
+    lng: -122.08427,
+  }
+
   return (
     <div className="App">
+       <MapSection location={location} zoomLevel={17} /> {/* include it here */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
